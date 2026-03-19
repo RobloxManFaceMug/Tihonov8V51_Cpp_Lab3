4428df99165339f7813353b26d4248f665e5644c
RobloxManFaceMug <nbahtnxohob@gmail.com>
2026-03-20 00:15:02 +0700

    Genshin Impact McDonalds meal

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <string>
 #include "acronym"
 #include "IsPalindrome"
-#include "Scrabble"
+#include "Scrabble2"
 
 int main(){
     std::string s = "laser";
-    std::vector<std::string> words = {"laser", "amplification", "stimulated", "emission", "radiation"};
-    if (IsAcronym(s, words)) {
+    std::vector<std::string> words1 = {"laser", "amplification", "stimulated", "emission", "radiation"};
+    if (IsAcronym(s, words1)) {
         std::cout << s << " is an acronym" << std::endl;
     }
     else {
@@ -23,6 +23,9 @@ int main(){
         std::cout << s2 << " is NOT a palindrome" << std::endl; 
     }
 
-    std::vector<std::string> s3 = {"i", "beloath", "microsoft", "excel"};
-    std::cout << "These words would normally get you " << ScrabbleScore(s3) << " point(s) in Scrabble" << std::endl;
+    std::vector<std::string> words = {"i", "beloath", "microsoft", "excel"};
+    std::vector<char> letters = {'i', 'e', 'l', 'o', 'b', 'x', 'x', 'd', 't', 'h', 'a', 'm', 'c', 's', 'f', 't'};
+    std::vector<int> score = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    int result = EvilScrabbleScore(words, letters, score);
+    std::cout << "The highest possible word would get you: " << result << " points in Scrabble";
 }
